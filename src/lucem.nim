135e681a81bd6617c2bfd088bb432a8e1a72c5e7
xTrayambak <xtrayambak@gmail.com>
2025-10-30 12:20:11 +0530

    disable patch loader for now

--- src/lucem.nim
+++ src/lucem.nim
@@ -1,17 +1,17 @@
 import pkg/owlkettle, pkg/owlkettle/adw
 import pkg/chronicles
-import application, meta, resource_loader, patch_loader
+import application, meta, resource_loader
 
 logScope:
   topics = "main"
 
-const content = staticRead "patches/old-get-up.lpat.js"
+# const content = staticRead "patches/old-get-up.lpat.js"
 
 proc main() {.inline.} =
   info "Lucem is now starting up", version = meta.Version
-  var loader: PatchLoader
-  loader.loadPatch("skibidi.lpat.js", content)
-  loader.execute()
+  # var loader: PatchLoader
+  #loader.loadPatch("skibidi.lpat.js", content)
+  # loader.execute()
 
   loadAppAssets()
   runLucemApp()
