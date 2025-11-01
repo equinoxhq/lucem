119b5e9f28468b1fe221a11930feba636ebb7c1e
xTrayambak <xtrayambak@gmail.com>
2025-11-01 17:16:48 +0530

    fix: compile time error

--- src/adw/about.nim
+++ src/adw/about.nim
@@ -1,28 +1,25 @@
-import pkg/owlkettle,
-       pkg/owlkettle/adw,
-       ../build_info
+import pkg/owlkettle, pkg/owlkettle/adw
+import ../meta
 
-template openAboutMenu*(app) =
-  discard app.open: gui:
-    AboutWindow:
-      applicationName = "Lucem"
-      developerName = "The EquinoxHQ Team"
-      version = meta.Version
-      supportUrl = "https://discord.gg/Z5m3n9fjcU"
-      issueUrl = "https://github.com/equinoxhq/lucem/issues"
-      website = "https://equinoxhq.github.io"
-      copyright =
-        """
+proc openAboutMenu*(app: Viewable) =
+  discard app.open:
+    gui:
+      AboutWindow:
+        applicationName = "Lucem"
+        developerName = "The EquinoxHQ Team"
+        version = meta.Version
+        supportUrl = "https://discord.gg/Z5m3n9fjcU"
+        issueUrl = "https://github.com/equinoxhq/lucem/issues"
+        website = "https://equinoxhq.github.io"
+        copyright =
+          """
 Copyright (C) 2025 xTrayambak and the EquinoxHQ Team
       """
-      license = meta.License
-      licenseType = LicenseMIT_X11
-      applicationIcon = "lucem"
-      developers = @["Trayambak (xTrayambak)"]
-      designers = @["Adrien (AshtakaOOf)"]
-      artists = @[]
-      documenters = @[]
-      credits =
-        @{
-          "Emotional support (probably)": @["Kirby (k1yrix)"],
-        }
+        license = meta.License
+        licenseType = LicenseMIT_X11
+        applicationIcon = "lucem"
+        developers = @["Trayambak (xTrayambak)"]
+        designers = @["Adrien (AshtakaOOf)"]
+        artists = @[]
+        documenters = @[]
+        credits = @{"Emotional support (probably)": @["Kirby (k1yrix)"]}
