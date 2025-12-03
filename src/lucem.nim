0d6b0405665f735d96ebb6a46f86352443eb21fb
xTrayambak <xtrayambak@gmail.com>
2025-12-03 16:15:51 +0530

    add: lockfile and splashes

--- src/lucem.nim
+++ src/lucem.nim
@@ -1,4 +1,4 @@
-import std/os
+import std/[random, os]
 import pkg/owlkettle, pkg/owlkettle/adw
 import pkg/[chronicles, shakar]
 import application, meta, patch_loader, sober_dir
@@ -10,6 +10,11 @@ const content = staticRead "patches/old-get-up.lpat.json"
 
 proc main() {.inline.} =
   info "Lucem is now starting up", version = meta.Version
+
+  # This is so professional
+  randomize()
+  echo sample(meta.Splashes)
+
   var loader: PatchLoader
   loader.loadPatch("old-get-up.lpat.js", content, official = true)
   loader.execute()
--- src/meta.nim
+++ src/meta.nim
@@ -8,4 +8,9 @@ const
 
   CommitHash* = gorge("git describe --tags --long --dirty")
   License* = staticRead("../LICENSE")
-  Splashes* = ["Have you ever had a dream?"]
+  Splashes* = [
+    "Have you ever had a dream?", "\"i hate owlkettle\" - ashtaka, the art of ui",
+    "atomics there and channels here",
+    "do you think because you are, or are you because you think?",
+    "https://media.discordapp.net/attachments/1285901307433058348/1445727403220406393/speechmeme-2025-12-03T10-43-45.png?ex=69316611&is=69301491&hm=616e33b1082836030172091edf4c43bbee82b573853969bb4e0abef21b99ddd4&=&format=webp&quality=lossless&width=665&height=841",
+  ]
